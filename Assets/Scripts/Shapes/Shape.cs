028dc7fac9ee9e743a202b98f652bf2c37400d50
rhunt2911 <huntrachel.work@gmail.com>
2026-03-23 11:44:47 +0000

    Created Script for Shape Instantiation in Game Space
    Script instantiates previously created game shapes in game world, with individual squares in relative position to one another. Square individuality in shape creation will be useful later once shape is placed on grid and is no longer defined as a specific shape itself

---
+++ Assets/Scripts/Shapes/Shape.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    public GameObject squareShapeImg;
+
+    //[HideInInspector]
+    public ShapeData currShapeData;
+
+    private List<GameObject> currShape = new List<GameObject>();
+    
+    void Start()
+    {
+        //test for method
+        RequestNewShape(currShapeData);
+    }
+
+    //counts number of active squares in current shape
+    private int GetSquareCount(ShapeData sd)
+    {
+        int count = 0;
+
+        foreach(var row in sd.shapeBoard)
+        {
+            foreach(var active in row.column)
+            {
+                if (active) { count++; }
+            }
+        }
+
+        return count;
+    }
+
+    public void RequestNewShape(ShapeData sd)
+    {
+        CreateShape(sd);
+    }
+    
+    //creates game shape
+    public void CreateShape(ShapeData sd)
+    {
+        currShapeData = sd;
+        int squareCount = GetSquareCount(sd);
+
+        //adds square images to list
+        while(currShape.Count <= squareCount)
+        {
+            currShape.Add(Instantiate(squareShapeImg, transform) as GameObject);
+        }
+
+        //positions square images to make shape
+        foreach(var square in currShape)
+        {
+            //positions all squares in shape at zero so can be positioned correctly when actually forming shape
+            square.gameObject.transform.position = Vector3.zero;
+            square.gameObject.SetActive(false);
+        }
+
+        var squareRect = squareShapeImg.GetComponent<RectTransform>();
+        var moveDistance = new Vector2(squareRect.rect.width * squareRect.localScale.x,
+            squareRect.rect.height * squareRect.localScale.y);
+
+        int currListIndex = 0;
+
+        //set square positions to form shape
+        for(int row = 0; row < sd.rowCount; row++)
+        {
+            for(int col = 0; col < sd.columnCount; col++)
+            {
+                //if loop at active square in shape
+                if (sd.shapeBoard[row].column[col])
+                {
+                    currShape[currListIndex].SetActive(true);
+                    currShape[currListIndex].GetComponent<RectTransform>().localPosition =
+                        new Vector2(GetSquareXPos(sd, col, moveDistance), GetSquareYPos(sd, row, moveDistance));
+
+                    currListIndex++;
+                }
+            }
+        }
+    }
+
+    //calculate x and y pos for each square in shape
+    private float GetSquareXPos(ShapeData sd, int col, Vector2 moveDistance)
+    {
+        //allows squares to move horizontally on the grid while maintaining the same shape
+        float xShift = 0f;
+        //int middleSquareIndex;
+        //int mult;
+
+        //calculates vertical position
+        if(sd.columnCount > 1)
+        {
+            float xStartPos;
+            
+            //odd number of squares in width
+            if (sd.columnCount % 2 != 0)
+            {
+                xStartPos = (sd.columnCount / 2) * moveDistance.x * -1;
+            }
+
+            //even number of squares in width
+            else
+            {
+                xStartPos = ((sd.columnCount / 2) - 1) * moveDistance.x * -1 - moveDistance.x / 2;
+            }
+
+            xShift = xStartPos + col * moveDistance.x;
+        }
+
+        return xShift;
+    }
+
+    private float GetSquareYPos(ShapeData sd, int row, Vector2 moveDistance)
+    {
+        float yShift = 0f;
+        //int middleSquareIndex;
+        //int mult;
+
+        //calculates horizontal position
+        if (sd.rowCount > 1)
+        {
+            float yStartPos;
+            
+            //odd number of squares in height
+            if(sd.rowCount % 2 != 0)
+            {
+                yStartPos = (sd.rowCount / 2) * moveDistance.y;
+            }
+
+            //even number of squares in height
+            else
+            {
+                yStartPos = ((sd.rowCount / 2) - 1) * moveDistance.y + moveDistance.y / 2;
+            }
+
+            yShift = yStartPos - row * moveDistance.y;
+        }
+
+        return yShift;
+    }
+}
