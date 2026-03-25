eac9a0019868d358793f42619452c54def443b05
rhunt2911 <huntrachel.work@gmail.com>
2026-03-25 16:17:34 +0000

    3 Shapes now Instantaited on Start
    3 random shapes from list instantiated on start, board and shapes in fitting position for testing (final position subject to change)

--- Assets/Scripts/Shapes/ShapeStorage.cs
+++ Assets/Scripts/Shapes/ShapeStorage.cs
@@ -7,10 +7,15 @@ public class ShapeStorage : MonoBehaviour
     public List<ShapeData> shapeDataList;
     public List<Shape> shapeList;
 
-    private float[] rotationsList = {0.0f, 90.0f, 180.0f, 270.0f}; 
+    private float[] rotationsList = {0.0f, 90.0f, 180.0f, 270.0f};
+
+    //public Vector3 startPos = new Vector3(0.0f, 0.0f, 0.0f);
+
+    //public int spaceBetweenShapes = 0;
     
     void Start()
     {
+        
         foreach(var shape in shapeList)
         {
             //get index of random shape
@@ -20,9 +25,19 @@ public class ShapeStorage : MonoBehaviour
 
             float rotation = rotationsList[UnityEngine.Random.Range(0, rotationsList.Length - 1)];
 
+            //Debug.Log("width of shape: " + width + ", height of shape: " + height);
 
-            shape.GetComponent<RectTransform>().transform.Rotate(0.0f, 0.0f, rotation, Space.Self);
-            Debug.Log("setting shape rotation to " + rotation);
+            var shapeRect = shape.GetComponent<RectTransform>();
+
+            int width = 100 * shapeDataList[shapeIndex].columnCount;
+            int height = 100 * shapeDataList[shapeIndex].rowCount;
+
+            shapeRect.sizeDelta = new Vector2(width, height);
+
+            shapeRect.transform.Rotate(0.0f, 0.0f, rotation, Space.Self);
+
+            //Debug.Log("setting shape rotation to " + rotation);
+
         }
     }
 
