604f4d6ee0116b846b9fb3d02eee608a8d84daaa
rhunt2911 <huntrachel.work@gmail.com>
2026-06-09 10:17:51 +0100

    Made and tested validity of empty shapes size 7
    Also improved neatness of shape manager script for unity inspector

--- Assets/Scripts/ShapeManager.cs
+++ Assets/Scripts/ShapeManager.cs
@@ -6,17 +6,25 @@ public class ShapeManager : MonoBehaviour
 {
     //TODO: make neater starting positions for shapes
     public Vector3Int[] startingShapePositions;
+    [Space] //added spaces to help visually in inspector
+    [Space]
 
     public List<GameObject> playableShapes; //shapes that can be played from the start of the game
     public List<GameObject> hardPlayableShapes; //shapes that will be added to playable shapes once player reaches certain score
     public GameObject playableShapesHolder;
+    [Space]
+    [Space]
 
     public List<GameObject> currPlayableShapes;
+    [Space]
+    [Space]
 
     public List<GameObject> emptySpaces; //shapes that are the negative space the players must try and make borders around
     public List<GameObject> mediumEmptySpaces; //shapes that will be added to empty spaces once player reaches a certain score
     public List<GameObject> hardEmptySpaces; //shapes that will be added to empty spaces once player reaches a certain, higher score
     public GameObject emptySpaceHolder;
+    [Space]
+    [Space]
 
     public GameObject currEmptySpace; //the current negative space the player must try and make a border around
     public Vector3Int emptySpaceDisplayPos; //where to display the visualisation of the shape the player needs to make 
